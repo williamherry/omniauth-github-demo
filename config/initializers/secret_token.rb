cdfe618e165324fcebfaa952119b997a13bef00b
William Herry <WilliamHerryChina@Gmail.com>
2013-07-29 12:15:20 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmniauthGithubDemo::Application.config.secret_key_base = 'd0b9852d3cc4fd191678fafcadea473b54d9a63b2e46011a005749ec50232cd218a1236d32015b9946253367b4bd84cfc5d8b90bd8f752d49ffed983572ebd50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OmniauthGithubDemo::Application.config.session_store :cookie_store, key: '_omniauth-github-demo_session'
